95e3d6c955d0824c23537965f9ca5337ef1b0148
csltx <hnatushakandri11@gmail.com>
2023-02-23 14:00:00 +0200

    Fix memory leak in Rust smart contract

--- escrow.sol
+++ escrow.sol
@@ -4,3 +4,4 @@
 // Add fallback mechanism in Solidity contract
 // Implement adaptive block size for Ethereum
 // Enhance privacy features using zk-STARKs
+// Improve error handling in transaction flow
