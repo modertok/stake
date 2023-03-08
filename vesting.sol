578a14060e37337f394d0b695ea631a8f2f170fc
csltx <hnatushakandri11@gmail.com>
2023-03-08 14:00:00 +0200

    Enhance security checks

--- vesting.sol
+++ vesting.sol
@@ -1,3 +1,4 @@
 // Add support for zk-SNARKs in smart contracts
 // Optimize storage for off-chain data
 // Fix interoperability issues with other blockchains
+// Optimize storage for off-chain data
