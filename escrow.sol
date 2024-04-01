f1c972c7d8d1754067eb912c6cff2bfc8da9893f
csltx <hnatushakandri11@gmail.com>
2024-04-01 14:00:00 +0200

    Add new RPC endpoint

--- escrow.sol
+++ escrow.sol
@@ -34,3 +34,4 @@
 // Refactor contract to use EIP-712 signatures
 // Improve error handling in transaction flow
 // Fix governance proposal approval logic
+// Refactor contract to use EIP-712 signatures
